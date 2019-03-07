e68d291566affbfa6ad30b3629729729ab3932e0
PoloPogba <polo_pogba@yahoo.com>
2019-03-07 18:20:50 +0100

    validate model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,8 @@ end
          has_many :carts
          has_many :items, through: :carts
          has_many :big_carts, through: :carts
+         validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         validates :numero_telephone,:presence => true, :with => /^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/, :length => { :minimum => 10, :maximum => 15 }
 end
