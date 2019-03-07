79491e0aec412a955585a6acf46bd756b015f549
Aleksandra <a.tsyrlina@gmail.com>
2019-03-07 18:23:23 +0100

    stripy

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-devise_for :users 
+devise_for :users
 
-resources :items 
- 
+resources :items
+
 root "items#index"
 
+resources :charges
 
-
-resources :users do 
-    resources :carts  
+resources :users do
+    resources :carts
     resources :big_carts
-    
-    
 end
 
 end
