e9132d3adf86e6e2e6b5df0c5060caa0740a1bed
PoloPogba <polo_pogba@yahoo.com>
2019-03-10 11:41:03 +0100

    trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ end
 
 # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, 
+  devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
 
          has_many :orders
