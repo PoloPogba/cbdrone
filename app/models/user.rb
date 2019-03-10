a4a2af2d22d975ce9786c3ae394760234d67869c
PoloPogba <polo_pogba@yahoo.com>
2019-03-10 11:15:26 +0100

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ end
 
 # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :trackable,
+  devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :validatable
 
          has_many :orders
--- db/schema.rb
+++ db/schema.rb
@@ -63,11 +63,6 @@ ActiveRecord::Schema.define(version: 2019_03_05_164211) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
     t.string "first_name"
     t.string "last_name"
     t.string "adresse"
